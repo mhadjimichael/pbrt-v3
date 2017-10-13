58164b93f6b8c60efea85131fb44fc684ccf0394
Marios Hadjimichael <marioshadj@gmail.com>
2017-10-12 23:15:55 -0700

    Compiled on macOS

---
+++ CMakeFiles/pbrt_test.dir/DependInfo.cmake
@@ -0,0 +1,62 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/marios/PTaaS/pbrt-v3/src/tests/analytic_scenes.cpp" "/Users/marios/PTaaS/pbrt-v3/CMakeFiles/pbrt_test.dir/src/tests/analytic_scenes.cpp.o"
+  "/Users/marios/PTaaS/pbrt-v3/src/tests/animatedtransform.cpp" "/Users/marios/PTaaS/pbrt-v3/CMakeFiles/pbrt_test.dir/src/tests/animatedtransform.cpp.o"
+  "/Users/marios/PTaaS/pbrt-v3/src/tests/bitops.cpp" "/Users/marios/PTaaS/pbrt-v3/CMakeFiles/pbrt_test.dir/src/tests/bitops.cpp.o"
+  "/Users/marios/PTaaS/pbrt-v3/src/tests/bounds.cpp" "/Users/marios/PTaaS/pbrt-v3/CMakeFiles/pbrt_test.dir/src/tests/bounds.cpp.o"
+  "/Users/marios/PTaaS/pbrt-v3/src/tests/bsdfs.cpp" "/Users/marios/PTaaS/pbrt-v3/CMakeFiles/pbrt_test.dir/src/tests/bsdfs.cpp.o"
+  "/Users/marios/PTaaS/pbrt-v3/src/tests/fileutil.cpp" "/Users/marios/PTaaS/pbrt-v3/CMakeFiles/pbrt_test.dir/src/tests/fileutil.cpp.o"
+  "/Users/marios/PTaaS/pbrt-v3/src/tests/find_interval.cpp" "/Users/marios/PTaaS/pbrt-v3/CMakeFiles/pbrt_test.dir/src/tests/find_interval.cpp.o"
+  "/Users/marios/PTaaS/pbrt-v3/src/tests/fourierbsdf.cpp" "/Users/marios/PTaaS/pbrt-v3/CMakeFiles/pbrt_test.dir/src/tests/fourierbsdf.cpp.o"
+  "/Users/marios/PTaaS/pbrt-v3/src/tests/fp_tests.cpp" "/Users/marios/PTaaS/pbrt-v3/CMakeFiles/pbrt_test.dir/src/tests/fp_tests.cpp.o"
+  "/Users/marios/PTaaS/pbrt-v3/src/tests/gtest/gtest-all.cc" "/Users/marios/PTaaS/pbrt-v3/CMakeFiles/pbrt_test.dir/src/tests/gtest/gtest-all.cc.o"
+  "/Users/marios/PTaaS/pbrt-v3/src/tests/gtest/gtest_main.cc" "/Users/marios/PTaaS/pbrt-v3/CMakeFiles/pbrt_test.dir/src/tests/gtest/gtest_main.cc.o"
+  "/Users/marios/PTaaS/pbrt-v3/src/tests/hair.cpp" "/Users/marios/PTaaS/pbrt-v3/CMakeFiles/pbrt_test.dir/src/tests/hair.cpp.o"
+  "/Users/marios/PTaaS/pbrt-v3/src/tests/hg.cpp" "/Users/marios/PTaaS/pbrt-v3/CMakeFiles/pbrt_test.dir/src/tests/hg.cpp.o"
+  "/Users/marios/PTaaS/pbrt-v3/src/tests/imageio.cpp" "/Users/marios/PTaaS/pbrt-v3/CMakeFiles/pbrt_test.dir/src/tests/imageio.cpp.o"
+  "/Users/marios/PTaaS/pbrt-v3/src/tests/parallel.cpp" "/Users/marios/PTaaS/pbrt-v3/CMakeFiles/pbrt_test.dir/src/tests/parallel.cpp.o"
+  "/Users/marios/PTaaS/pbrt-v3/src/tests/print.cpp" "/Users/marios/PTaaS/pbrt-v3/CMakeFiles/pbrt_test.dir/src/tests/print.cpp.o"
+  "/Users/marios/PTaaS/pbrt-v3/src/tests/sampling.cpp" "/Users/marios/PTaaS/pbrt-v3/CMakeFiles/pbrt_test.dir/src/tests/sampling.cpp.o"
+  "/Users/marios/PTaaS/pbrt-v3/src/tests/shapes.cpp" "/Users/marios/PTaaS/pbrt-v3/CMakeFiles/pbrt_test.dir/src/tests/shapes.cpp.o"
+  "/Users/marios/PTaaS/pbrt-v3/src/tests/spectrum.cpp" "/Users/marios/PTaaS/pbrt-v3/CMakeFiles/pbrt_test.dir/src/tests/spectrum.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GOOGLE_GLOG_DLL_DECL="
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "src/ext/openexr/IlmBase/Imath"
+  "src/ext/openexr/IlmBase/Half"
+  "src/ext/openexr/IlmBase/Iex"
+  "src/ext/openexr/OpenEXR/IlmImf"
+  "src/ext/openexr/IlmBase/config"
+  "src/ext/openexr/OpenEXR/config"
+  "src/ext/glog/src"
+  "src/ext/glog"
+  "src/ext/ptex/src/ptex"
+  "src"
+  "src/core"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/marios/PTaaS/pbrt-v3/CMakeFiles/pbrt.dir/DependInfo.cmake"
+  "/Users/marios/PTaaS/pbrt-v3/src/ext/openexr/OpenEXR/IlmImf/CMakeFiles/IlmImf.dir/DependInfo.cmake"
+  "/Users/marios/PTaaS/pbrt-v3/src/ext/openexr/IlmBase/Imath/CMakeFiles/Imath.dir/DependInfo.cmake"
+  "/Users/marios/PTaaS/pbrt-v3/src/ext/openexr/IlmBase/Half/CMakeFiles/Half.dir/DependInfo.cmake"
+  "/Users/marios/PTaaS/pbrt-v3/src/ext/glog/CMakeFiles/glog.dir/DependInfo.cmake"
+  "/Users/marios/PTaaS/pbrt-v3/src/ext/ptex/src/ptex/CMakeFiles/Ptex_static.dir/DependInfo.cmake"
+  "/Users/marios/PTaaS/pbrt-v3/src/ext/openexr/IlmBase/IlmThread/CMakeFiles/IlmThread.dir/DependInfo.cmake"
+  "/Users/marios/PTaaS/pbrt-v3/src/ext/openexr/IlmBase/Iex/CMakeFiles/Iex.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake_install.cmake
@@ -0,0 +1,105 @@
+# Install script for directory: /Users/marios/PTaaS/pbrt-v3
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "/Users/marios/PTaaS/pbrt-v3/pbrt")
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/pbrt" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/pbrt")
+    if(CMAKE_INSTALL_DO_STRIP)
+      execute_process(COMMAND "/Library/Developer/CommandLineTools/usr/bin/strip" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/pbrt")
+    endif()
+  endif()
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "/Users/marios/PTaaS/pbrt-v3/bsdftest")
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/bsdftest" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/bsdftest")
+    if(CMAKE_INSTALL_DO_STRIP)
+      execute_process(COMMAND "/Library/Developer/CommandLineTools/usr/bin/strip" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/bsdftest")
+    endif()
+  endif()
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "/Users/marios/PTaaS/pbrt-v3/imgtool")
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/imgtool" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/imgtool")
+    if(CMAKE_INSTALL_DO_STRIP)
+      execute_process(COMMAND "/Library/Developer/CommandLineTools/usr/bin/strip" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/imgtool")
+    endif()
+  endif()
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "/Users/marios/PTaaS/pbrt-v3/obj2pbrt")
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/obj2pbrt" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/obj2pbrt")
+    if(CMAKE_INSTALL_DO_STRIP)
+      execute_process(COMMAND "/Library/Developer/CommandLineTools/usr/bin/strip" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/obj2pbrt")
+    endif()
+  endif()
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "/Users/marios/PTaaS/pbrt-v3/cyhair2pbrt")
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/cyhair2pbrt" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/cyhair2pbrt")
+    if(CMAKE_INSTALL_DO_STRIP)
+      execute_process(COMMAND "/Library/Developer/CommandLineTools/usr/bin/strip" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/cyhair2pbrt")
+    endif()
+  endif()
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "/Users/marios/PTaaS/pbrt-v3/libpbrt.a")
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libpbrt.a" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libpbrt.a")
+    execute_process(COMMAND "/Library/Developer/CommandLineTools/usr/bin/ranlib" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libpbrt.a")
+  endif()
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("/Users/marios/PTaaS/pbrt-v3/src/ext/openexr/cmake_install.cmake")
+  include("/Users/marios/PTaaS/pbrt-v3/src/ext/glog/cmake_install.cmake")
+  include("/Users/marios/PTaaS/pbrt-v3/src/ext/ptex/cmake_install.cmake")
+
+endif()
+
+if(CMAKE_INSTALL_COMPONENT)
+  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
+else()
+  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
+endif()
+
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+file(WRITE "/Users/marios/PTaaS/pbrt-v3/${CMAKE_INSTALL_MANIFEST}"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
